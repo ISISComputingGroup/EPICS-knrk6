ca990e61280eeeba2239ec6522507c90dc7fc6e4
Jack Harper <jack.harper@stfc.ac.uk>
2024-08-06 14:42:02 +0100

    run ruff format

--- system_tests/lewis_emulators/knrk6/__init__.py
+++ system_tests/lewis_emulators/knrk6/__init__.py
@@ -2,4 +2,4 @@ from ..lewis_versions import LEWIS_LATEST
 from .device import SimulatedKnrk6
 
 framework_version = LEWIS_LATEST
-__all__ = ['SimulatedKnrk6']
+__all__ = ["SimulatedKnrk6"]
--- system_tests/lewis_emulators/knrk6/device.py
+++ system_tests/lewis_emulators/knrk6/device.py
@@ -6,7 +6,6 @@ from .states import DefaultState
 
 
 class SimulatedKnrk6(StateMachineDevice):
-
     def _initialize_data(self):
         """
         Initialize all of the device's attributes.
@@ -15,19 +14,18 @@ class SimulatedKnrk6(StateMachineDevice):
         self.input_correct = True
 
         self.position = 1  # Home position
-        self.valve_type = 'VALVE 6'
+        self.valve_type = "VALVE 6"
 
     def _get_state_handlers(self):
         return {
-            'default': DefaultState(),
+            "default": DefaultState(),
         }
 
     def _get_initial_state(self):
-        return 'default'
+        return "default"
 
     def _get_transition_handlers(self):
-        return OrderedDict([
-        ])
+        return OrderedDict([])
 
     def reset(self):
         self._initialize_data()
